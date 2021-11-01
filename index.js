2447256e910bce22751abb7ae17aee6c4f52daff
Nicholas Shephard <shephardn@gmail.com>
2021-11-01 12:27:27 -0500

    Assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-
 // Write your code here
+
+//multiply
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+//random
+const random = (Math.round((Math.random() * 10 + 1)));
+
+//mod
+let num3 = 14;
+let num4 = 10;
+const mod = num3 % num4;
+
+//max
+let num5 = 1;
+let num6 = 2;
+let num7 = 20;
+const max = Math.max(num5, num5, num7);
